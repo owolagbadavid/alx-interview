016da9bb3131f8dc2bebafdf4580a56f8519831a
DAVID <owolagbadavid@gmail.com>
2024-04-11 20:05:11 +0100

    intial commit

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+"""
+Pascal's Triangle
+"""
+
+
+def pascal_triangle(n):
+    """Create a function def pascal_triangle(n): that returns a list of lists
+    of integers representing the Pascal’s triangle of n
+    """
+    if n <= 0:
+        return []
+
+    pascal = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(pascal[i - 1][j - 1] + pascal[i - 1][j])
+        row.append(1)
+        pascal.append(row)
+    return pascal
